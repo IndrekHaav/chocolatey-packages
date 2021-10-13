e40c12f38ae0574610cbb51461421acd5e88dbdb
Indrek Haav <indrek.haav@hotmail.com>
2021-10-13 22:13:32 +0300

    Added Buckets (portable and install version) https://www.budgetwithbuckets.com/

---
+++ manual/buckets.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url = 'https://github.com/buckets/application/releases/download/v0.60.1/Buckets-Setup-0.60.1.exe'
+$checksum = '3384604b06d0ad8050d5e61f398dba69cc7c65c595a8509d1d66463f473fc2fc'
+
+Install-ChocolateyPackage -PackageName $Env:ChocolateyPackageName `
+    -Url $url -Checksum $checksum -ChecksumType 'sha256' `
+    -FileType 'exe' -silentArgs '/S' -validExitCodes @(0)
---
+++ manual/buckets.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url = 'https://github.com/buckets/application/releases/download/v0.60.1/Buckets-0.60.1.exe'
+$checksum = '88332ffe369d5fc61dc36c5c722b58826e7d88fd017efb7c231614fbde3e9ad1'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyWebFile -PackageName $Env:ChocolateyPackageName `
+    -Url $url -Checksum $checksum -ChecksumType 'sha256' `
+    -FileFullPath $toolsDir\buckets.exe
