dcc0aee406650c18424a5474b779307405c3051d
Indrek Haav <IndrekHaav@users.noreply.github.com>
2021-11-09 12:08:39 +0200

    Use splatting in Buckets (portable) script refs #1

--- manual/buckets.portable/tools/chocolateyinstall.ps1
+++ manual/buckets.portable/tools/chocolateyinstall.ps1
@@ -1,9 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url = 'https://github.com/buckets/application/releases/download/v0.60.1/Buckets-0.60.1.exe'
-$checksum = '88332ffe369d5fc61dc36c5c722b58826e7d88fd017efb7c231614fbde3e9ad1'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile -PackageName $Env:ChocolateyPackageName `
-    -Url $url -Checksum $checksum -ChecksumType 'sha256' `
-    -FileFullPath $toolsDir\buckets.exe
+$packageArgs = @{
+    PackageName     = $Env:ChocolateyPackageName
+    Url             = 'https://github.com/buckets/application/releases/download/v0.60.1/Buckets-0.60.1.exe'
+    Checksum        = '88332ffe369d5fc61dc36c5c722b58826e7d88fd017efb7c231614fbde3e9ad1'
+    ChecksumType    = 'sha256'
+    FileFullPath    = Join-Path $toolsDir 'buckets.exe'
+}
+
+Get-ChocolateyWebFile @packageArgs
