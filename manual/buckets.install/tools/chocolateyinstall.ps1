51dfdbac5bd4a58a6f4a662adb3ada41c55aa7b6
Indrek Haav <IndrekHaav@users.noreply.github.com>
2021-11-09 12:08:07 +0200

    Use splatting in Buckets (install) script refs #1

--- manual/buckets.install/tools/chocolateyinstall.ps1
+++ manual/buckets.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url = 'https://github.com/buckets/application/releases/download/v0.60.1/Buckets-Setup-0.60.1.exe'
-$checksum = '3384604b06d0ad8050d5e61f398dba69cc7c65c595a8509d1d66463f473fc2fc'
+$packageArgs = @{
+    PackageName     = $Env:ChocolateyPackageName
+    Url             = 'https://github.com/buckets/application/releases/download/v0.60.1/Buckets-Setup-0.60.1.exe'
+    Checksum        = '3384604b06d0ad8050d5e61f398dba69cc7c65c595a8509d1d66463f473fc2fc'
+    ChecksumType    = 'sha256'
+    FileType        = 'exe'
+    silentArgs      = '/S'
+    validExitCodes  = @(0)
+}
 
-Install-ChocolateyPackage -PackageName $Env:ChocolateyPackageName `
-    -Url $url -Checksum $checksum -ChecksumType 'sha256' `
-    -FileType 'exe' -silentArgs '/S' -validExitCodes @(0)
+Install-ChocolateyPackage @packageArgs
