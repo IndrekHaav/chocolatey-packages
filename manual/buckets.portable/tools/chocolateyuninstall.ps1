da192c2762929142f5b78de4fa21445fb733d3dd
Indrek Haav <IndrekHaav@users.noreply.github.com>
2022-04-11 15:47:16 +0300

    Improve Buckets portable uninstall script Previous version caused errors when uninstalling without having ever launched the app

--- manual/buckets.portable/tools/chocolateyuninstall.ps1
+++ manual/buckets.portable/tools/chocolateyuninstall.ps1
@@ -5,5 +5,5 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 # Remove additional files created by the program
 $files = @('app.sqlite', 'log.log', 'state.json')
 $files | ForEach-Object {
-    Join-Path $toolsDir $_ | Remove-Item
+    Join-Path $toolsDir $_ | Remove-Item -ErrorAction SilentlyContinue
 }
