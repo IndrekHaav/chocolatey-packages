adf2baca72559a439abce648335f02c6b14df487
Indrek Haav <IndrekHaav@users.noreply.github.com>
2022-04-15 18:16:15 +0300

    Updated Buckets packages with new version

--- manual/buckets.install/tools/chocolateyinstall.ps1
+++ manual/buckets.install/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     PackageName     = $Env:ChocolateyPackageName
-    Url             = 'https://github.com/buckets/application/releases/download/v0.66.0/Buckets-Setup-0.66.0.exe'
-    Checksum        = '06d0c18997732695207dba1f80d9ef70ef151f22124451cd3ec995bcad6ab3a2'
+    Url             = 'https://github.com/buckets/application/releases/download/v0.66.2/Buckets-Setup-0.66.2.exe'
+    Checksum        = 'bc636055d0674175e61ddbac726d70acdf8a95f48576155fb0632a7c81ae003c'
     ChecksumType    = 'sha256'
     FileType        = 'exe'
     silentArgs      = '/S'
--- manual/buckets.portable/tools/chocolateyinstall.ps1
+++ manual/buckets.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName     = $Env:ChocolateyPackageName
-    Url             = 'https://github.com/buckets/application/releases/download/v0.66.0/Buckets-0.66.0.exe'
-    Checksum        = '05153f03d014df2b34baa97fa84ae47b4cf34800d8d6e7552832731cd475ba84'
+    Url             = 'https://github.com/buckets/application/releases/download/v0.66.2/Buckets-0.66.2.exe'
+    Checksum        = '847b8d9dc545c5b294651606a52ffbeba5c280e0dd854ac0964387d8d7b15a59'
     ChecksumType    = 'sha256'
     FileFullPath    = Join-Path $toolsDir 'buckets.exe'
 }
