4bbd59474a638cde70b799b48dd38c4896934de6
Indrek Haav <IndrekHaav@users.noreply.github.com>
2022-09-14 10:12:36 +0300

    Updated Tea package with new version

--- packages/tea/tools/chocolateyinstall.ps1
+++ packages/tea/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName     = $Env:ChocolateyPackageName
-    Url             = 'https://dl.gitea.io/tea/0.8.0/tea-0.8.0-windows-386.exe'
-    Checksum        = 'afd76615ae5b5eaf36cd8b4b9a79840efddc4a778e9e35c7b1c6f94af8f55e37'
+    Url             = 'https://dl.gitea.io/tea/0.9.0/tea-0.9.0-windows-386.exe'
+    Checksum        = '792e0ae2baa6a5ac4c3fef7c19a5489777ea8c1ba3e29044560f16af85c642a2'
     ChecksumType    = 'sha256'
-    Url64bit        = 'https://dl.gitea.io/tea/0.8.0/tea-0.8.0-windows-amd64.exe'
-    Checksum64      = '4dba79d63ae2c0ffa623c090d1e64edec38fbfe7206dae6d6fc3d2b465dfc55c'
+    Url64bit        = 'https://dl.gitea.io/tea/0.9.0/tea-0.9.0-windows-amd64.exe'
+    Checksum64      = '1f4d0f4edeec04381811bf888035d20442b43d98ea39743d1661c2e42d1c79ec'
     ChecksumType64  = 'sha256'
     FileFullPath    = Join-Path $toolsDir 'tea.exe'
 }
